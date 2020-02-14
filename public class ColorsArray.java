ac6544d3baddce7958b0c2afc2aca63ec20b5e40
Simon Courtecuisse <simoncourtecuisse@gmail.com>
2020-02-14 10:43:31 +0100

    First commit

---
+++ public class ColorsArray.java
@@ -0,0 +1,17 @@
+public class ColorsArray {
+    public static void main(String[] args){  
+        //TODO- Déclarez une variable nommée colors qui correspond à un tableau de String de longueur 5
+        String[] colors = new String []{"red","yellow", "orange", "green", "blue"};
+        //TODO- Remplissez le tableau avec les couleurs demandées
+        
+        //TODO- Remplacez 'green' par 'emerald' dans le tableau
+        colors[3]="emerald";
+      
+        //Affiche le contenu du tableau
+        for(String color:colors){
+            System.out.println(color);
+        si (true) { System.out.println(color);}
+        
+        } 
+    }
+}
